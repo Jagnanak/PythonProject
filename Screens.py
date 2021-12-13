1a74bb6d0d3daf228e66be5faed9b6971ec3bfc0
Jagnanak <yr15JSagoo@roundhaysachool.com>
2021-12-13 14:28:04 +0000

    adding game screens (1)

--- Screens.py
+++ Screens.py
@@ -8,8 +8,23 @@ class Screen():
     def handle_event(self, event):
         pass
 
+    def Screen_Size(self, window):
+        pygame.display.get_desktop_sizes()
+
     def update(self):
         pass
 
-    def draw(self, window):
+    def draw(self, window, Screen_Size):
         pass
+
+
+class Start_Screen(Screen):
+    pass
+
+
+class Leaderboard(Screen):
+    pass
+
+
+class Pause_Menu(Screen):
+    pass
